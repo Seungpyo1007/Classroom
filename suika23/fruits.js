011ff2088476bde7439fc616c94a3ea27e7fe6f0
Seungpyo1007 <rush94434@gmail.com>
2024-06-21 11:02:05 +0900

    2

---
+++ suika23/fruits.js
@@ -0,0 +1,48 @@
+const FRUITS= [
+  {
+    name: "/image/00_cherry",
+    radius: 33 / 2,
+  },
+  // name: "/suika/img/00_cherry", 다른 곳에서 작업할 경우 경로 확인
+  {
+    name: "/image/01_strawberry",
+    radius: 48 / 2,
+  },
+  {
+    name: "/image/02_grape",
+    radius: 61 / 2,
+  },
+  {
+    name: "/image/03_gyool",
+    radius: 69 / 2,
+  },
+  {
+    name: "/image/04_orange",
+    radius: 89 / 2,
+  },
+  {
+    name: "/image/05_apple",
+    radius: 114 / 2,
+  },
+  {
+    name: "/image/06_pear",
+    radius: 129 / 2,
+  },
+  {
+    name: "/image/07_peach",
+    radius: 156 / 2,
+  },
+  {
+    name: "/image/08_pineapple",
+    radius: 177 / 2,
+  },
+  {
+    name: "/image/09_melon",
+    radius: 220 / 2,
+  },
+  {
+    name: "/image/10_watermelon",
+    radius: 259 / 2,
+  },
+];
+export { FRUITS };
--- suika23/main.js
+++ suika23/main.js
@@ -1,3 +1,6 @@
+import { FRUITS } from './fruits.js';
+
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -41,6 +44,7 @@ const ground = Bodies.rectangle(310, 820, 620, 60, {
 
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic: true,
+    isSenser : true, // 충돌은 감지하는데, 물리엔진은 적용 안함
     render: { fillStyle: '#E6B143'}
 });
 
@@ -50,3 +54,34 @@ World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+
+
+function addFruit() {
+    // 과일 인덱스값 저장
+    const index = Math.floor(Math.random() * 5);
+    
+    // 과일 이미지 경로 불러오기
+    const fruit = FRUITS[index];
+
+    const body = Bodies.circle(300, 50, fruit.radius, {
+        index : index,
+        isSleeping : true, // 시작시 바로 떨어지지 않고 잠시 멈춤
+        render : {
+            sprite : { texture : `${fruit.name}.png`}
+        },
+
+        // 통통 튀기는 설정
+        restitution : 1.0
+        
+    })
+
+    currentBody = body;
+    currentFruit = fruit;
+
+    World.add(world, body)
+}
+
+addFruit();
